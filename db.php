18dc271a026b30d978c2f5ab40dd9e11a8d6434d
Yogendra Neeladri <115712703+YogendraNeeladri@users.noreply.github.com>
2024-11-07 14:58:11 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,17 +1,17 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$db = "ecommerce";
+$servername = "dpg-csm8daogph6c73aeec90-a";
+$port = "5432";
+$username = "electromart_md9x_user";
+$password = "i8NIwaleuY0La76F4nMLcqIapg4qiLH1";
+$db = "electromart_md9x";
 
 // Create connection
-$con = mysqli_connect($servername, $username, $password,$db);
+$con = mysqli_connect($servername, $username, $password, $db, $port);
 
 // Check connection
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
 
 ?>
