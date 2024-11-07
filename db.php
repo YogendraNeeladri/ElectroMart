2766450825d1c087726b43214419d821ff9bfd3e
Yogendra Neeladri <115712703+YogendraNeeladri@users.noreply.github.com>
2024-11-07 14:58:40 +0530

    Update db.php

--- db.php
+++ db.php
@@ -6,7 +6,7 @@ $username = "electromart_md9x_user";
 $password = "i8NIwaleuY0La76F4nMLcqIapg4qiLH1";
 $db = "electromart_md9x";
 
-// Create connection
+// Create connectio
 $con = mysqli_connect($servername, $username, $password, $db, $port);
 
 // Check connection
